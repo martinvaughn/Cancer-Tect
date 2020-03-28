55650d1096d9d5e0c5eba6efa558ca43d503e073
Minhoru <mijay_1998@hotmail.com>
2020-03-27 23:55:57 -0600

    prediction activity changes

--- app/src/main/java/com/e/cancer_tect/PredictionActivity.java
+++ app/src/main/java/com/e/cancer_tect/PredictionActivity.java
@@ -1,10 +1,16 @@
 package com.e.cancer_tect;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class PredictionActivity extends AppCompatActivity {
+
+    private String prediction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,9 +18,21 @@ public class PredictionActivity extends AppCompatActivity {
         setContentView(R.layout.activity_prediction);
 
         //get analysis from the intent;
-        //Intent intent = getIntent();
-        //String prediction = intent.getStringExtra("analysis");
-    }
+        Intent intent = getIntent();
+        prediction = intent.getStringExtra("analysis");
 
+
+        if (prediction == "Malignant") {
+            TextView noCancer = (TextView)findViewById(R.id.noCancer);
+            noCancer.setVisibility(View.VISIBLE);
+        } else {
+            TextView cancer = (TextView)findViewById(R.id.cancer);
+            cancer.setVisibility(View.VISIBLE);
+        }
+
+        Button doctor = findViewById(R.id.doctor);
+        doctor.setVisibility(View.VISIBLE);
+
+    }
     //display analysis -> System.out(prediction);
 }

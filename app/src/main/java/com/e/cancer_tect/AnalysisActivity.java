20a177a5d310a980bce7848ddfd09a08ba428be1
Sengthay Kaing <kai19001@byui.edu>
2020-03-28 14:57:21 -0600

    Update AnalysisActivity.java

--- app/src/main/java/com/e/cancer_tect/AnalysisActivity.java
+++ app/src/main/java/com/e/cancer_tect/AnalysisActivity.java
@@ -70,5 +70,24 @@ public class AnalysisActivity extends AppCompatActivity {
         intent.putExtra("Prediction", prediction);
         startActivity(intent);
     }
-
 }
+
+
+
+/*************************************
+ Bitmap bitmap = getYourInputImage();
+ bitmap = Bitmap.createScaledBitmap(bitmap, 224, 224, true);
+
+ int batchNum = 0;
+ float[][][][] input = new float[1][224][224][3];
+ for (int x = 0; x < 224; x++) {
+ for (int y = 0; y < 224; y++) {
+ int pixel = bitmap.getPixel(x, y);
+ // Normalize channel values to [-1.0, 1.0]. This requirement varies by
+ // model. For example, some models might require values to be normalized
+ // to the range [0.0, 1.0] instead.
+ input[batchNum][x][y][0] = (Color.red(pixel) - 127) / 128.0f;
+ input[batchNum][x][y][1] = (Color.green(pixel) - 127) / 128.0f;
+ input[batchNum][x][y][2] = (Color.blue(pixel) - 127) / 128.0f;
+ *********************************************/
+
